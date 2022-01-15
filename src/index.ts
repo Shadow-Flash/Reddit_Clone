98d1173c4fa58a8532e2bcf8abc2962772895f6c
Shadow-Flash <varung848@gmail.com>
2022-01-15 14:39:59 +0530

    [ADDED] GraphQL added with graphql server

--- src/index.ts
+++ src/index.ts
@@ -1,15 +1,29 @@
 import { MikroORM } from "@mikro-orm/core";
 import { __prod__ } from "./contants";
-import { Post } from "./entities/Post";
+// import { Post } from "./entities/Post";
 import mikroOrmConfig from "./mikro-orm.config";
+import express from 'express';
+import { ApolloServer } from "apollo-server-express";
+import { buildSchema } from "type-graphql";
+import { HelloResolver } from "./resolvers/hello";
 
 const main = async() => {
     const orm = await MikroORM.init(mikroOrmConfig)
     await orm.getMigrator().up();
-    // const post = orm.em.create(Post,{title: 'my first post'});
-    // await orm.em.persistAndFlush(post);
-    // const post = await orm.em.find(Post,{});
-    // console.log(post);
+    
+    const app = express();
+    const apolloServer = new ApolloServer({
+        schema: await buildSchema({
+            resolvers: [HelloResolver],
+            validate: false
+        })
+    })
+    await apolloServer.start();
+    apolloServer.applyMiddleware({ app })
+
+    app.listen(2022, () => {
+        console.log('server started on localhost:2022');
+    })
 }
 
 main();
